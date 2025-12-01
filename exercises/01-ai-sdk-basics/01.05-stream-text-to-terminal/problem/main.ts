675f3bb7098b4c2d4179e5f51facf154e7305967
Nicolas Wormser <gh@nwo.pm>
2025-12-01 18:55:32 +0000

    01.05 stream text to terminal

--- exercises/01-ai-sdk-basics/01.05-stream-text-to-terminal/problem/main.ts
+++ exercises/01-ai-sdk-basics/01.05-stream-text-to-terminal/problem/main.ts
@@ -1,12 +1,11 @@
 import { google } from '@ai-sdk/google';
 import { streamText } from 'ai';
 
-const model = google('gemini-2.0-flash');
+const model = google('gemini-2.0-flash',);
 
-const prompt =
-  'Give me the first paragraph of a story about an imaginary planet.';
+const prompt = 'Give me the first paragraph of a story about an imaginary planet.';
 
-const stream = TODO; // TODO - stream some text with the model above.
+const stream = streamText({ model, prompt })
 
 for await (const chunk of stream.textStream) {
   process.stdout.write(chunk);
