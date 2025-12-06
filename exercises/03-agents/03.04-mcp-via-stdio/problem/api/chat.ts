8d200fe822db26deb2ecfd1b8830243e89c42966
Nicolas Wormser <gh@nwo.pm>
2025-12-06 20:20:47 +0000

    feat: 03.04 mcp via stdio

--- exercises/03-agents/03.04-mcp-via-stdio/problem/api/chat.ts
+++ exercises/03-agents/03.04-mcp-via-stdio/problem/api/chat.ts
@@ -16,9 +16,21 @@ export const POST = async (req: Request): Promise<Response> => {
   const body: { messages: UIMessage[] } = await req.json();
   const { messages } = body;
 
-  // TODO - create an MCP client that uses the StdioMCPTransport
-  // to connect to the GitHub MCP server
-  const mcpClient = TODO;
+  const transportConfig = new StdioMCPTransport({
+    command: "docker",
+    args: [
+      "run",
+      "-i",
+      "--rm",
+      "-e",
+      "GITHUB_PERSONAL_ACCESS_TOKEN",
+      "ghcr.io/github/github-mcp-server",
+    ],
+    env: {
+      GITHUB_PERSONAL_ACCESS_TOKEN: process.env.GITHUB_PERSONAL_ACCESS_TOKEN!,
+    },
+  })
+  const mcpClient = await createMCPClient({ transport: transportConfig });
 
   const result = streamText({
     model: google('gemini-2.5-flash'),
@@ -26,15 +38,11 @@ export const POST = async (req: Request): Promise<Response> => {
     system: `
       You are a helpful assistant that can use the GitHub API to interact with the user's GitHub account.
     `,
-    // TODO - use the mcpClient.tools() method to get the tools
-    tools: TODO,
+    tools: await mcpClient.tools(),
     stopWhen: [stepCountIs(10)],
   });
 
   return result.toUIMessageStreamResponse({
-    // TODO - use the mcpClient.close() method to close the MCP client
-    // when the stream is finished. This will also close the process
-    // running the GitHub MCP server.
-    onFinish: TODO,
+    onFinish: async () => await mcpClient.close(),
   });
 };
