01d2b2e7597d6c73d959e642e1d74323621a9755
Nicolas Wormser <gh@nwo.pm>
2025-12-07 13:09:12 +0000

    feat: 05.02 basic context engineering prompting

--- exercises/05-context-engineering/05.02-basic-prompting/problem/main.ts
+++ exercises/05-context-engineering/05.02-basic-prompting/problem/main.ts
@@ -7,12 +7,25 @@ const INPUT = `Do some research on induction hobs and how I can replace a 100cm 
 
 const result = await streamText({
   model: google('gemini-2.0-flash-lite'),
-  // TODO: Rewrite this prompt using the Anthropic template from
-  // the previous exercise.
-  // You will NOT need all of the sections from the template.
   prompt: `
-    Generate me a title:
-    ${INPUT}
+<task-context>
+You are an helpful assistant who generates titles for conversations.
+</task-context>
+
+Here is the initial chat prompt:
+<history>
+  ${INPUT}
+</history>
+
+<rules>
+  Here are some important rules for the task:
+  - Find the most concise title that accurately sums up the conversation.
+  - Always return titles that are between 18 and 30 characters longs
+</rules>
+
+Generate a title for the conversation.
+
+Return only the title.
   `,
 });
 
